061fd3f1c6625c2c80cbb78473369e86c5d692e6
quang709 <69231857+quang709@users.noreply.github.com>
2020-10-02 14:14:07 +0700

    Xây dựng lớp QuadraticEquation

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace Xây_dựng_lớp_QuadraticEquation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Nhập a: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập c: ");
+            double c = double.Parse(Console.ReadLine());
+            QuadraticEquation qe = new QuadraticEquation(a, b, c);
+
+            if (qe.GetDiscriminant() > 0)
+            {
+                Console.WriteLine("Phương trình có 2 nghiệm x1 = {0}, x2 = {1}: ", qe.GetRoot2x1(), qe.GetRoot2x2());
+            }
+            else if (qe.GetDiscriminant() == 0)
+            {
+                Console.WriteLine("Phương trình có một nghiệm duy nhất: " + qe.GetRoot1());
+            }
+            else
+                Console.WriteLine("Phương trình vô nghiệm");
+        }
+    }
+}
---
+++ QuadraticEquation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xây_dựng_lớp_QuadraticEquation
+{
+    class QuadraticEquation
+    {
+        private double a { set; get; }
+        private double b { set; get; }
+        private double c { set; get; }
+
+        public QuadraticEquation()
+        {
+
+        }
+        public QuadraticEquation(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public double GetDiscriminant()
+        {
+            double delta;
+            return delta = b * b - (4 * a * c);
+        }
+        public double GetRoot1()
+        {
+            return -b / (2 * a);
+        }
+        public double GetRoot2x1()
+        {
+            return (-b + Math.Pow(GetDiscriminant(), 0.5)) / (2 * a);
+        }
+
+        public double GetRoot2x2()
+        {
+            return (-b - Math.Pow(GetDiscriminant(), 0.5)) / (2 * a);
+        }
+    }
+}
